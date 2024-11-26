c0f8faea90898990e28fe7da44f670bb272bb4dc
JYKIMkentech <deu04152@kentech.ac.kr>
2024-11-26 19:04:01 +0900

    .
    .

--- SOC/Real Data/UDDS_Parsing.m
+++ SOC/Real Data/UDDS_Parsing.m
@@ -15,10 +15,10 @@ ocv_values = soc_ocv(:, 2);  % OCV 값
 
 %% 3. SOC 계산
 % 초기 SOC 설정
-SOC_initial = 0.9901;  % 초기 SOC를 설정합니다.
+SOC_initial = 0.9907;  % 초기 SOC를 설정합니다.
 
 % 배터리 용량 (Ah를 Coulomb로 변환)
-Q_battery = 2.9 * 3600;  % 배터리 용량 (2.9Ah)
+Q_battery = 2.7742 * 3600;  % 배터리 용량 (2.9Ah)
 
 % 시간에 따른 SOC 계산 (trapz를 사용하여 전류 적분)
 udds_SOC = zeros(size(udds_time));
